eac9d27697cce19f12ed1607ac0ccb7663e04211
Christopher Paul <124709632+cpaul-mech@users.noreply.github.com>
2025-02-13 22:30:47 -0700

    I believe tis working, the toolbar is displaying properly

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+    return (
+        <div>
+            <header>
+                {/* <!-- Navigation elements --> */}
+                <nav>
+                    {/* <!-- Menu is a semantic alternative to <ul> that represents an interaction --> */}
+                    <menu class="navbar-nav">
+                        <li class="nav-item" id="eightBall"><a href="index.html">🔮</a></li>
+                        <li class="nav-item"><a href="questionAndAnswer.html">Ask Question</a></li>
+                        <li class="nav-item"><a href="yourStats.html">User History</a></li>
+                        <li class="nav-item"><a href="about.html">About</a></li>
+                    </menu>
+                </nav>
+            </header>
+            <main>App components go here</main>
+            <footer className="bg-dark text-white-50">
+                <div className="container-fluid">
+                    <span className="text-reset">Author Name(s)</span>
+                    <a className="text-reset" href="https://github.com/webprogramming260/simon-react">
+                        Source
+                    </a>
+                </div>
+            </footer>
+        </div>
+    );
 }
