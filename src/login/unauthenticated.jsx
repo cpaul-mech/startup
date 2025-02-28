4c7ef74a371647ba11e4c2b8bcc218a1635e4f99
Christopher Paul <124709632+cpaul-mech@users.noreply.github.com>
2025-02-27 20:22:57 -0700

    further edits to login and unauthenticated

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -19,25 +19,25 @@ export function Unauthenticated(props) {
     }
 
     return (
-        <>
-            <div>
-                <div className='input-group mb-3'>
-                    <span className='input-group-text'>@</span>
-                    <input className='form-control' type='text' value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='your@email.com' />
-                </div>
-                <div className='input-group mb-3'>
-                    <span className='input-group-text'>🔒</span>
-                    <input className='form-control' type='password' onChange={(e) => setPassword(e.target.value)} placeholder='password' />
-                </div>
-                <Button variant='primary' onClick={() => loginUser()} disabled={!userName || !password}>
-                    Login
-                </Button>
-                <Button variant='secondary' onClick={() => createUser()} disabled={!userName || !password}>
-                    Create
-                </Button>
-            </div>
+        <main>
 
-            <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
-        </>
+            <h1 id="Welcome_statement">Welcome to the COSMIC 8 BALL
+                <span>🔮</span>
+                {/* <!-- Some kind of Interesting Graphic of a Cosmic 8 Ball --> */}
+            </h1>
+            <img id="eightBallImg" src="Chris_Cosmic_8_Ball.png" alt="Cosmic-8 Ball graphic"
+                width="500"></img>
+            <h1>The COSMIC 8 Ball Needs No Introduction</h1>
+            <h2>But you do. Please login below.</h2>
+                <div className="input-group mb-3">
+                    <input id="emailInput" className="form-control" type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="your@email.com" />
+                </div>
+                <div className="input-group mb-3">
+                    <input id="passwordInput" className="form-control" type="password" onChange={(e) => setPassword(e.target.value)} placeholder="password" />
+                </div>
+                <Button type="submit" className="btn btn-primary" onClick={()=> loginUser()} disabled={!userName || !password}>Login</Button>
+                <Button type="submit" className="btn btn-secondary" onClick={()=>createUser()} disabled={!userName || !password}>Create</Button>
+            
+        </main>
     );
 }
